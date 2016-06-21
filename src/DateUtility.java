d548148604dbbd5ad186ab81a592ffb87399ef09
oracle <oracle@GBTC441R11RH.montgomerycollege.edu>
2016-06-21 10:14:44 -0400

    initial commit

---
+++ src/DateUtility.java
@@ -0,0 +1,18 @@
+
+public class DateUtility {
+	int month;
+	int day;
+	int year;
+	
+	String displayCalendar(int month, int year){
+		return null;}
+	
+	int getLastDayOfMonth(int month, int year){
+		return year;} 
+	
+	String toString(int month, int day, int year, String delimiter){
+		return delimiter;} //prints the date as mm/dd/yyyy
+	
+	boolean isLeapYear(int year){
+		return false;}
+}
